49ffb08e6f21d39b59d8b12c5288f7d7ca96b5d5
korin0912 <>
2022-06-11 22:50:30 +0900

    本一覧の ListView を MainWindow から分離

---
+++ BookListView.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KindleViewer
+{
+    public partial class BookListView : UserControl
+    {
+        private Kindle kindle = null;
+
+        private ListView listView = null;
+        private ScrollViewer listScrollViewer = null;
+
+        public BookListView(Kindle kindle)
+        {
+            Log.Info("bookList start");
+
+            DataContext = kindle;
+
+            InitializeComponent();
+
+            this.kindle = kindle;
+
+            this.Loaded += (s, e) =>
+            {
+                Log.Info("InitializeComponent loaded");
+
+                listView = this.FindName("BookListView_ListView") as ListView;
+                if (listView == null)
+                {
+                    Log.Error("BookListView not found.");
+                    return;
+                }
+
+                listView.SizeChanged += (s, e) =>
+                {
+                    UpdateScroll();
+                };
+
+                listScrollViewer = listView.FindDescendant<ScrollViewer>();
+                Log.Info($"ScrollViewer get {(listScrollViewer != null ? "success" : "fail")}");
+                if (listScrollViewer != null)
+                {
+                    listScrollViewer.ScrollChanged += (s, e) =>
+                    {
+                        UpdateScroll();
+                    };
+                }
+
+                UpdateScroll();
+            };
+        }
+
+        /// <summary>
+        /// スクロール時更新
+        /// </summary>
+        private void UpdateScroll()
+        {
+            if (listView.Items.Count <= 0)
+            {
+                return;
+            }
+
+            // 画面に表示されている本のインデクス計算
+            var listSize = listView.DesiredSize;
+
+            var item = listView.FindDescendant<ListViewItem>();
+            var itemSize = item != null ? item.DesiredSize : new Size(100, 200);
+
+            var scroll = listScrollViewer != null ? listScrollViewer.VerticalOffset : 0;
+
+            var vCount = (int)Math.Floor(listSize.Width / itemSize.Width);
+
+            var begin = (int)Math.Floor(scroll / itemSize.Height) * vCount;
+            var end = ((int)Math.Floor((scroll + listSize.Height) / itemSize.Height) + 1) * vCount - 1;
+
+            begin = Math.Clamp(begin, 0, listView.Items.Count - 1);
+            end = Math.Clamp(end, 0, listView.Items.Count - 1);
+
+            // Log.Info($"{begin} - {end}, {listSize.Width}, {listSize.Height}, {itemSize.Width}");
+
+            // カバー画像ロード
+            for (var i = begin; i <= end; i++)
+            {
+                if (!kindle.Books[i].IsCoverImageLoaded)
+                {
+                    kindle.Books[i].LoadCoverImage();
+                }
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,95 +22,26 @@ namespace KindleViewer
     {
         private Kindle kindle = new Kindle();
 
-        private Book[] Books => kindle.Books;
-
-        private ListView bookListView = null;
-        private ScrollViewer bookListScrollViewer = null;
-
         public MainWindow()
         {
             Log.Info("start");
 
-            kindle.ReadCache();
-
             ImageLoader.Instance.Start();
 
-            DataContext = kindle;
+            kindle.ReadCache();
+
             InitializeComponent();
 
             this.Loaded += (s, e) =>
             {
-                Log.Info("InitializeComponent loaded");
-
-                bookListView = this.FindName("BookListView") as ListView;
-                if (bookListView == null)
-                {
-                    Log.Error("BookListView not found.");
-                    return;
-                }
-
-                bookListView.Loaded += (s, e) =>
-                {
-                    Log.Info("BookListView loaded");
-
-                    UpdateScroll(bookListView, bookListScrollViewer);
-
-                    bookListScrollViewer = bookListView.FindDescendant<ScrollViewer>();
-                    Log.Info($"ScrollViewer get {(bookListScrollViewer != null ? "success" : "fail")}");
-                    if (bookListScrollViewer != null)
-                    {
-                        bookListScrollViewer.ScrollChanged += (s, e) =>
-                        {
-                            UpdateScroll(bookListView, bookListScrollViewer);
-                        };
-                    }
-                };
-
-                bookListView.SizeChanged += (s, e) =>
-                {
-                    UpdateScroll(bookListView, bookListScrollViewer);
-                };
-
+                // 本一覧追加
+                var layoutDocumentPane = this.FindName("LayoutDocumentPane") as AvalonDock.Layout.LayoutDocumentPane;
+                var layoutDocument = new AvalonDock.Layout.LayoutDocument();
+                layoutDocument.Title = "  一覧  ";
+                layoutDocument.CanClose = false;
+                layoutDocument.Content = new BookListView(kindle);
+                layoutDocumentPane.Children.Add(layoutDocument);
             };
-        }
-
-        /// <summary>
-        /// スクロール時更新
-        /// </summary>
-        /// <param name="listView"></param>
-        private void UpdateScroll(ListView listView, ScrollViewer scrollViewer)
-        {
-            if (listView.Items.Count <= 0)
-            {
-                return;
-            }
-
-            // 画面に表示されている本のインデクス計算
-            var listSize = listView.DesiredSize;
-
-            var item = listView.FindDescendant<ListViewItem>();
-            var itemSize = item != null ? item.DesiredSize : new Size(100, 200);
-
-            var scroll = scrollViewer != null ? scrollViewer.VerticalOffset : 0;
-
-            var vCount = (int)Math.Floor(listSize.Width / itemSize.Width);
-
-            var begin = (int)Math.Floor(scroll / itemSize.Height) * vCount;
-            var end = ((int)Math.Floor((scroll + listSize.Height) / itemSize.Height) + 1) * vCount - 1;
-
-            begin = Math.Clamp(begin, 0, listView.Items.Count - 1);
-            end = Math.Clamp(end, 0, listView.Items.Count - 1);
-
-            // Log.Info($"{begin} - {end}, {listSize.Width}, {listSize.Height}, {itemSize.Width}");
-
-            // カバー画像ロード
-            for (var i = begin; i <= end; i++)
-            {
-                if (!kindle.Books[i].IsCoverImageLoaded)
-                {
-                    kindle.Books[i].LoadCoverImage();
-                }
-            }
         }
     }
 }
